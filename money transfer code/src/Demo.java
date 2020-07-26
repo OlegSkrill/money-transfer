870b19eb4324a1cc2ca9db1363b2719fff1416ba
Oleg Matveenko <om1190@yandex.ru>
2020-07-26 11:50:02 +0500

    first

---
+++ money transfer code/src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        long balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        long total = balance + transfer;
+        System.out.println(total);
+    }
+}
+
